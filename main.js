e0b7f71c69676583ee2d2cb28aa71b8037c8e094
asurance <784672616@qq.com>
2020-09-11 10:54:55 +0000

    Deploying to AI from  @ a17ad5c20a13fece415235c64f0daca42e55346d 🚀

--- main.js
+++ main.js
@@ -86,6 +86,132 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/baseCreep.ts":
+/*!**************************!*\
+  !*** ./src/baseCreep.ts ***!
+  \**************************/
+/*! exports provided: EnergyMap, GetRequiredEnergy */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "EnergyMap", function() { return EnergyMap; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "GetRequiredEnergy", function() { return GetRequiredEnergy; });
+const EnergyMap = {
+    move: 50,
+    work: 100,
+    carry: 50,
+    attack: 80,
+    ranged_attack: 150,
+    heal: 250,
+    claim: 600,
+    tough: 10
+};
+function GetRequiredEnergy(body) {
+    return body.reduce((pre, cur) => {
+        return pre + EnergyMap[cur];
+    }, 0);
+}
+
+
+/***/ }),
+
+/***/ "./src/carrier.ts":
+/*!************************!*\
+  !*** ./src/carrier.ts ***!
+  \************************/
+/*! exports provided: Carrier */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Carrier", function() { return Carrier; });
+/* harmony import */ var _baseCreep__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./baseCreep */ "./src/baseCreep.ts");
+var _a;
+
+const Carrier = (_a = class Carrier {
+        static serialize() {
+            return this.id;
+        }
+        static deserialize(data) {
+            this.id = data;
+        }
+        create(spawn) {
+            const name = `${Carrier.type}-${Carrier.id}`;
+            const result = spawn.spawnCreep(['work', 'move'], `${Carrier.type}-${Carrier.id}`);
+            if (result === OK) {
+                Carrier.id++;
+                this.creep = Game.creeps[name];
+                this.creep.memory = {
+                    type: "carrier" /* Carrier */,
+                    pickup: true,
+                    targetId: null
+                };
+            }
+            return result;
+        }
+        ticker() {
+            const memory = this.creep.memory;
+            if (memory.pickup) {
+                if (this.creep.store.getFreeCapacity('energy') > 0) {
+                    if (memory.targetId === null) {
+                        const target = this.creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
+                        if (target) {
+                            memory.targetId = target.id;
+                        }
+                    }
+                    if (memory.targetId !== null) {
+                        this.creep.say('寻找资源');
+                        const target = Game.getObjectById(memory.targetId);
+                        if (this.creep.pickup(target) === ERR_NOT_IN_RANGE) {
+                            this.creep.moveTo(target);
+                        }
+                    }
+                    else {
+                        memory.pickup = false;
+                        memory.targetId = null;
+                    }
+                }
+                else {
+                    memory.pickup = false;
+                    memory.targetId = null;
+                }
+            }
+            else {
+                if (this.creep.store.getUsedCapacity('energy') > 0) {
+                    if (memory.targetId === null) {
+                        const target = this.creep.pos.findClosestByPath(FIND_MY_SPAWNS);
+                        if (target) {
+                            memory.targetId = target.id;
+                        }
+                    }
+                    if (memory.targetId !== null) {
+                        this.creep.say('放置资源');
+                        const target = Game.getObjectById(memory.targetId);
+                        if (this.creep.transfer(target, 'energy') === ERR_NOT_IN_RANGE) {
+                            this.creep.moveTo(target);
+                        }
+                    }
+                    else {
+                        memory.pickup = true;
+                        memory.targetId = null;
+                    }
+                }
+                else {
+                    memory.pickup = true;
+                    memory.targetId = null;
+                }
+            }
+        }
+    },
+    _a.id = 0,
+    _a.type = "carrier" /* Carrier */,
+    _a.minEnergy = Object(_baseCreep__WEBPACK_IMPORTED_MODULE_0__["GetRequiredEnergy"])(['carry', 'move']),
+    _a);
+
+
+/***/ }),
+
 /***/ "./src/index.ts":
 /*!**********************!*\
   !*** ./src/index.ts ***!
@@ -95,45 +221,79 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _walker__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./walker */ "./src/walker.ts");
+/* harmony import */ var _carrier__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./carrier */ "./src/carrier.ts");
+/* harmony import */ var _walker__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./walker */ "./src/walker.ts");
+/* harmony import */ var _worker__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./worker */ "./src/worker.ts");
 
-let count = 0;
+
+
+for (const key in Memory.creeps) {
+    if (!(key in Game.creeps)) {
+        delete Memory.creeps[key];
+    }
+}
+const creepCtorMap = {
+    worker: _worker__WEBPACK_IMPORTED_MODULE_2__["Worker"],
+    walker: _walker__WEBPACK_IMPORTED_MODULE_1__["Walker"],
+    carrier: _carrier__WEBPACK_IMPORTED_MODULE_0__["Carrier"],
+};
+for (const key in creepCtorMap) {
+    const ctor = creepCtorMap[key];
+    ctor.deserialize(Memory.ctors[key]);
+}
+const creepMap = new Map();
 for (const creepName in Game.creeps) {
     const creep = Game.creeps[creepName];
-    if (creep.memory.type === 'worker') {
-        const target = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
-        if (target) {
-            if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(target);
-            }
-        }
-        count++;
+    const type = creep.memory.type;
+    const handler = new creepCtorMap[type]();
+    handler.creep = creep;
+    if (creepMap.has(type)) {
+        creepMap.get(type).push(handler);
     }
-    else if (creep.memory.type === _walker__WEBPACK_IMPORTED_MODULE_0__["Walker"].type) {
-        _walker__WEBPACK_IMPORTED_MODULE_0__["Walker"].count++;
-        _walker__WEBPACK_IMPORTED_MODULE_0__["Walker"].tick(creep, creep.room);
+    else {
+        creepMap.set(type, [handler]);
     }
 }
 for (const spawnName in Game.spawns) {
-    const spawn = Game.spawns[spawnName];
-    if (!spawn.spawning && spawn.store['energy'] >= 200) {
-        if (_walker__WEBPACK_IMPORTED_MODULE_0__["Walker"].count > 0) {
-            const name = `worker_${count}`;
-            const result = spawn.spawnCreep(['work', 'move'], `worker_${count}`);
-            if (result === OK) {
-                Memory.creeps[name].type = 'worker';
-            }
-            else {
-                console.log(result);
-            }
-        }
-        else {
-            const result = _walker__WEBPACK_IMPORTED_MODULE_0__["Walker"].create(spawn, spawn.room);
+    const span = Game.spawns[spawnName];
+    const workerList = creepMap.get("worker" /* Worker */);
+    if (!workerList) {
+        if (!span.spawning && span.room.energyAvailable >= _worker__WEBPACK_IMPORTED_MODULE_2__["Worker"].minEnergy) {
+            const worker = new _worker__WEBPACK_IMPORTED_MODULE_2__["Worker"]();
+            const result = worker.create(span, span.room.energyAvailable);
             if (result !== OK) {
                 console.log(result);
+                break;
             }
         }
     }
+    const carrierList = creepMap.get("carrier" /* Carrier */);
+    if (!carrierList) {
+        if (!span.spawning && span.room.energyAvailable >= _carrier__WEBPACK_IMPORTED_MODULE_0__["Carrier"].minEnergy) {
+            const carrier = new _carrier__WEBPACK_IMPORTED_MODULE_0__["Carrier"]();
+            const result = carrier.create(span, span.room.energyAvailable);
+            if (result !== OK) {
+                console.log(result);
+                break;
+            }
+        }
+    }
+    const walkerList = creepMap.get("walker" /* Walker */);
+    if (!walkerList) {
+        if (!span.spawning && span.room.energyAvailable >= _walker__WEBPACK_IMPORTED_MODULE_1__["Walker"].minEnergy) {
+            const walker = new _walker__WEBPACK_IMPORTED_MODULE_1__["Walker"]();
+            const result = walker.create(span, span.room.energyAvailable);
+            if (result !== OK) {
+                console.log(result);
+                break;
+            }
+        }
+    }
+}
+creepMap.forEach(creeps => creeps.forEach(creep => creep.ticker()));
+for (const key in creepCtorMap) {
+    const ctor = creepCtorMap[key];
+    Memory.ctors[key] = ctor.serialize();
 }
 
 
@@ -149,81 +309,102 @@ for (const spawnName in Game.spawns) {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Walker", function() { return Walker; });
-const Walker = {
-    count: 0,
-    type: "Walker" /* Walker */,
-    create(spawn, room) {
-        const result = spawn.spawnCreep(['move'], 'walker');
-        if (result === OK) {
-            Memory.creeps['walker'].type = "Walker" /* Walker */;
+/* harmony import */ var _baseCreep__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./baseCreep */ "./src/baseCreep.ts");
+var _a;
+
+const Walker = (_a = class Walker {
+        static serialize() {
+            return this.id;
         }
-        Memory.creeps['walker'].direction = getNextDirection(spawn.pos, room);
-        return result;
+        static deserialize(data) {
+            this.id = data;
+        }
+        create(spawn) {
+            const name = `${Walker.type}-${Walker.id}`;
+            const result = spawn.spawnCreep(['move'], `${Walker.type}-${Walker.id}`);
+            if (result === OK) {
+                Walker.id++;
+                this.creep = Game.creeps[name];
+                this.creep.memory = {
+                    type: "walker" /* Walker */,
+                    direction: 1 + Math.floor(Math.random() * 8),
+                    restTick: 60,
+                };
+            }
+            return result;
+        }
+        ticker() {
+            const memory = this.creep.memory;
+            memory.restTick--;
+            if (memory.restTick < 0) {
+                memory.restTick = 60;
+                memory.direction = 1 + Math.floor(Math.random() * 8);
+            }
+            this.creep.move(memory.direction);
+        }
     },
-    tick(creep, room) {
-        let direction;
-        if (canMove(creep.pos, creep.memory.direction, room)) {
-            direction = creep.memory.direction;
+    _a.id = 0,
+    _a.type = "walker" /* Walker */,
+    _a.minEnergy = Object(_baseCreep__WEBPACK_IMPORTED_MODULE_0__["GetRequiredEnergy"])(['move']),
+    _a);
+
+
+/***/ }),
+
+/***/ "./src/worker.ts":
+/*!***********************!*\
+  !*** ./src/worker.ts ***!
+  \***********************/
+/*! exports provided: Worker */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Worker", function() { return Worker; });
+/* harmony import */ var _baseCreep__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./baseCreep */ "./src/baseCreep.ts");
+var _a;
+
+const Worker = (_a = class Worker {
+        static serialize() {
+            return this.id;
         }
-        else {
-            direction = getNextDirection(creep.pos, room);
-            creep.memory.direction = direction;
+        static deserialize(data) {
+            this.id = data;
         }
-        creep.move(direction);
-    }
-};
-function canMove(pos, dir, room) {
-    let dx = 0;
-    let dy = 0;
-    switch (dir) {
-        case LEFT:
-        case TOP_LEFT:
-        case BOTTOM_LEFT:
-            dx = -1;
-            break;
-        case RIGHT:
-        case TOP_RIGHT:
-        case BOTTOM_RIGHT:
-            dx = 1;
-            break;
-    }
-    switch (dir) {
-        case TOP:
-        case TOP_LEFT:
-        case TOP_RIGHT:
-            dy = -1;
-            break;
-        case BOTTOM:
-        case BOTTOM_LEFT:
-        case BOTTOM_RIGHT:
-            dy = 1;
-            break;
-    }
-    return validPosition(pos.x + dx, pos.y + dy, room);
-}
-function getNextDirection(pos, room) {
-    const dx = [0, 1, 1, 1, 0, -1, -1, -1];
-    const dy = [-1, -1, 0, 1, 1, 1, 0, -1];
-    const indice = [0, 1, 2, 3, 4, 5, 6, 7];
-    const ok = indice.filter(i => {
-        return validPosition(dx[i] + pos.x, dy[i] + pos.y, room);
-    });
-    return (TOP + ok[Math.random() * ok.length]);
-}
-function validPosition(x, y, room) {
-    if (x >= 0 && x < 50 && y >= 0 && y < 50) {
-        const result = room.lookAt(x, y);
-        if (result.every(r => r.terrain !== 'wall')) {
-            return true;
+        create(spawn) {
+            const name = `${Worker.type}-${Worker.id}`;
+            const result = spawn.spawnCreep(['work', 'move'], `${Worker.type}-${Worker.id}`);
+            if (result === OK) {
+                Worker.id++;
+                this.creep = Game.creeps[name];
+                this.creep.memory = {
+                    type: "worker" /* Worker */,
+                    targetId: null,
+                };
+            }
+            return result;
         }
-        else {
-            return false;
+        ticker() {
+            const memory = this.creep.memory;
+            if (memory.targetId === null) {
+                const target = this.creep.pos.findClosestByPath(FIND_SOURCES);
+                if (target) {
+                    memory.targetId = target.id;
+                }
+            }
+            if (memory.targetId !== null) {
+                this.creep.say('采矿中');
+                const target = Game.getObjectById(memory.targetId);
+                if (this.creep.harvest(target) === ERR_NOT_IN_RANGE) {
+                    this.creep.moveTo(target);
+                }
+            }
         }
-    }
-    else {
-        return false;
-    }
-}
+    },
+    _a.id = 0,
+    _a.type = "worker" /* Worker */,
+    _a.minEnergy = Object(_baseCreep__WEBPACK_IMPORTED_MODULE_0__["GetRequiredEnergy"])(['work', 'move']),
+    _a);
 
 
 /***/ })
