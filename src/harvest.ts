4a601122b17647c6f14fda85412d6525bafa39b4
yuchao.xie <yuchao.xie@alo7.com>
2020-09-17 13:10:27 +0800

    harvest添加超时判断

--- src/harvest.ts
+++ src/harvest.ts
@@ -2,14 +2,34 @@ import { RandomObjectInList } from './util'
 
 interface Harvestable extends MemoryData {
     harvestId?: Id<Source>
+    harvestTicker?: number
 }
 export function Harvest(creep: Creep<Harvestable>): number {
     let target: Source | null = null
     if (creep.memory.harvestId) {
         target = Game.getObjectById(creep.memory.harvestId)
     }
-    if (target === null) {
+    let needRefresh = false
+    if (target) {
+        if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
+            let ticker = creep.memory.harvestTicker ?? 180
+            ticker--
+            if (ticker <= 0) {
+                needRefresh = true
+                delete creep.memory.harvestTicker
+            } else {
+                creep.memory.harvestTicker = ticker
+            }
+        }
+    }
+    if (target === null || needRefresh) {
         const source = creep.room.find(FIND_SOURCES)
+        if (needRefresh) {
+            const index = source.indexOf(target!)
+            if (index >= 0) {
+                source.splice(index, 1)
+            }
+        }
         target = RandomObjectInList(source)
         if (target) {
             creep.memory.harvestId = target.id
