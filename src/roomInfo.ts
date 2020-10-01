e45737e89cfe6a9b170b912c677e59274568d76a
asurance <784672616@qq.com>
2020-10-01 20:46:18 +0800

    fix 置空error

--- src/roomInfo.ts
+++ src/roomInfo.ts
@@ -3,18 +3,18 @@
  * @param room 房间
  */
 export function GetRoomInfo(room: Room): RoomMemory {
-    if(!room.memory.sourceInfo){
+    if (!room.memory.sourceInfo) {
         room.memory.sourceInfo = room.find(FIND_SOURCES).map(source => source.id)
         room.memory.creepInfo = new Array<string | null>(room.memory.sourceInfo.length).fill(null)
     }
     return room.memory
 }
 
-export function OnCreepDead(room:Room, creepName: string): boolean {
+export function OnCreepDead(room: Room, creepName: string): boolean {
     if (room.memory.creepInfo) {
         const index = room.memory.creepInfo.indexOf(creepName)
         if (index >= 0) {
-            room.memory.creepInfo.splice(index, 1)
+            room.memory.creepInfo[index] = null
             return true
         } else {
             return false
