afd895cc71c97784a301c5ab349d026ae4a57a75
asurance <784672616@qq.com>
2020-10-01 09:34:28 +0000

    Deploying to AI from  @ e347afe541393c7f2d3e62040011afdce57ee40e 🚀

--- main.js
+++ main.js
@@ -870,24 +870,18 @@ exports.OnCreepDead = exports.GetRoomInfo = void 0;
  * @param room 房间
  */
 function GetRoomInfo(room) {
-    let info = room.memory;
-    if (!info) {
-        const sourceInfo = room.find(FIND_SOURCES).map(source => source.id);
-        info = {
-            sourceInfo,
-            creepInfo: new Array(sourceInfo.length).fill(null),
-        };
-        room.memory = info;
+    if (!room.memory.sourceInfo) {
+        room.memory.sourceInfo = room.find(FIND_SOURCES).map(source => source.id);
+        room.memory.creepInfo = new Array(room.memory.sourceInfo.length).fill(null);
     }
-    return info;
+    return room.memory;
 }
 exports.GetRoomInfo = GetRoomInfo;
 function OnCreepDead(room, creepName) {
-    const info = room.memory;
-    if (info) {
-        const index = info.creepInfo.indexOf(creepName);
+    if (room.memory.creepInfo) {
+        const index = room.memory.creepInfo.indexOf(creepName);
         if (index >= 0) {
-            info.creepInfo.splice(index, 1);
+            room.memory.creepInfo.splice(index, 1);
             return true;
         }
         else {
