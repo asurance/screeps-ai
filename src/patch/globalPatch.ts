70dd97f7b84e54d83eb2b751d4ce0f4944dde90d
yuchao.xie <yuchao.xie@alo7.com>
2020-10-20 18:33:53 +0800

    添加房间信息输出

--- src/patch/globalPatch.ts
+++ src/patch/globalPatch.ts
@@ -8,6 +8,12 @@ interface Stats {
     credit: number
 }
 
+interface RoomStats {
+    name: string
+    level: number
+    progress: number
+}
+
 global.GenerateStats = () => {
     const stats: Stats = {
         gcl: Math.floor(Game.gcl.progress / Game.gcl.progressTotal * 100),
@@ -19,4 +25,15 @@ global.GenerateStats = () => {
         credit: Game.market.credits,
     }
     console.log(JSON.stringify(stats, undefined, 4))
+    for (const roomName in Game.rooms) {
+        const room = Game.rooms[roomName]
+        if (room.controller) {
+            const roomStats: RoomStats = {
+                name: roomName,
+                level: room.controller.level,
+                progress: Math.floor(room.controller.progress / room.controller.progressTotal * 100)
+            }
+            console.log(JSON.stringify(roomStats, undefined, 4))
+        }
+    }
 }
