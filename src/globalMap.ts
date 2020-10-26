58d610f10b5cb2c4d913aaccef9a152a308fd521
yuchao.xie <yuchao.xie@alo7.com>
2020-10-26 15:23:04 +0800

    fix global reset带来的问题

--- src/globalMap.ts
+++ src/globalMap.ts
@@ -3,6 +3,8 @@ import { DefineGlobalCmd } from './util/util'
 export const SpawnMap = new Map<string, string[]>()
 export const CreepMap = new Map<string, string[]>()
 
+let isReset = true
+
 DefineGlobalCmd('SpawnMap', () => {
     SpawnMap.forEach((spawns, room) => {
         console.log(room, JSON.stringify(spawns))
@@ -14,7 +16,35 @@ DefineGlobalCmd('CreepMap', () => {
     })
 })
 
-export function Scan(): void {
+function InitMap() {
+    for (const spawnName in Game.spawns) {
+        const spawn = Game.spawns[spawnName]
+        const list = SpawnMap.get(spawn.room.name)
+        if (list) {
+            list.push(spawn.name)
+        } else {
+            SpawnMap.set(spawn.room.name, [spawn.name])
+        }
+        if (spawn.memory.roomName !== spawn.room.name) {
+            spawn.memory.roomName = spawn.room.name
+        }
+    }
+    for (const creepName in Game.creeps) {
+        const creep = Game.creeps[creepName]
+        const list = CreepMap.get(creep.room.name)
+        if (list) {
+            list.push(creep.name)
+        } else {
+            CreepMap.set(creep.room.name, [creep.name])
+        }
+        if (creep.memory.roomName !== creep.room.name) {
+            creep.memory.roomName = creep.room.name
+        }
+    }
+    isReset = false
+}
+
+function UpdateMap() {
     for (const spawnName in Game.spawns) {
         const spawn = Game.spawns[spawnName]
         if (spawn.memory.roomName !== spawn.room.name) {
@@ -52,5 +82,13 @@ export function Scan(): void {
             }
             creep.memory.roomName = creep.room.name
         }
+    }
+}
+
+export function Scan(): void {
+    if (isReset) {
+        InitMap()
+    } else {
+        UpdateMap()
     }
 }
